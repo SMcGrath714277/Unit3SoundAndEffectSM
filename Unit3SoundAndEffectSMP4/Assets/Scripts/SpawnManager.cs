14def90756eddff5706aea3c4faf8165ad3ab734
SMcGrath714277 <714277@student.dusd.net>
2023-03-24 11:38:32 -0700

    Update
    Completed Lesson 3.1 on the Unity Instructions.

---
+++ Unit3SoundAndEffectSMP4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public float startDelay = 2;
+    public float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+        Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+    }
+}
